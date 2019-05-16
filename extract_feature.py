d590aaf579525894a5c18e52dbc10321429a04d7
Pietro Alovisi <pietro.alovisi@mail.polimi.it>
2019-05-16 19:11:51 +0200

    Finish feature extracting according to paper

--- extract_feature.py
+++ extract_feature.py
@@ -1,12 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io import wavfile   
+import librosa
 import os
 import sys
 from lab1 import *
 
 Folders = ['IRMAS-TestingData-Part3/Part3/']
-
 
 FS = 44100 # sampling rate
 WINLEN =  882# 25 msec
@@ -38,79 +38,74 @@ def progress_bar(count, total, suffix=''):
     sys.stdout.flush()  # As suggested by Rom Ruben
 
 
-def main():
-
-    for folder in Folders:
-        print("Entering folder ", folder)
-
-        for root, dirs, files in os.walk(folder):
-            total_files = len(files)/2
-
-            count = 0
-
-            for file in files:
-                if file.endswith('.wav'):
-                    count += 1
-                    progress_bar(count, total_files, suffix='')
-                    feat = extract_from_file(folder+file)
-
-
-
-
-def extract_from_file(filename, preemp=False, window=False, fft_len=512):
-
-    #print(filename)
-    # read the data
+def extract_from_file(filename, preemp=False, window=False, fft_len=512, seconds=1):
+    # Read the data
     fs, data = wavfile.read(filename)
-    print(fs)
 
-    # extract feature on both the channels
+    # Take mean
+    mono = np.mean(data, axis=1) 
+    # Downsample
+    mono = mono[np.arange(0, mono.size, 2)]
+    # Normalize
+    mono /= np.max(np.abs(mono))
+    # Remove single dimensions
+    mono = np.squeeze(mono)
 
+    # Compute Short Time Fourier Transform
+    stft = np.abs(librosa.stft(mono, win_length=1024, hop_length=512, center=True))
+    # Convert to Mel Spectogram
+    mel_spec = librosa.feature.melspectrogram(S=stft, sr=22050, n_mels=128)
+    # Take the natural logarithm
+    ln_mel_spec = np.log(mel_spec + np.finfo(float).eps)
+
+    # Segementation of spectogram
+    seg_dur = 43 * seconds
+    spec_list = []
+    for idx in range(0, ln_mel_spec.shape[1] - seg_dur + 1, seg_dur):
+        spec_list.append(ln_mel_spec[:, idx:(idx+seg_dur)])
+    #print('Number of spectrograms:', len(spec_list))
+    X = np.expand_dims(np.array(spec_list), axis=1)
+    #print(X.shape)
+
+    # Extract feature on both the channels
     filename_cut = filename[:-4]
 
     res = {}
 
     res["filename"] = filename_cut
+    res["mono"]     = mono 
+    res["stft"]     = stft
+    res["mspec"]    = X
+    res["labels"]   = np.zeros([11])
 
-    res["fft"] = np.zeros([512,2])
-    res["mspec"] = np.zeros([40,2])
-    res["ceps"] = np.zeros([13,2])
-    res["lmfcc"] = np.zeros([13,2])
-
-    for channel in [0,1]:
-        channeldata = data[:,channel].T
-        channeldata.shape = (1, channeldata.size)
-
-        if preemp:
-            preemph = preemp(channeldata)
-        else:
-            preemph = channeldata
-        if window:
-            windowed = windowing(preemph)
-        else:
-            windowed = preemph
-        spec = powerSpectrum(windowed, fft_len)
-        mspec = logMelSpectrum(spec, FS)
-        ceps = cepstrum(mspec, 13)
-        lift = lifter(ceps, 22)
-
-        res["fft"][:,channel] = spec
-        res["mspec"][:,channel] = mspec
-        res["ceps"][:,channel] =  ceps
-        res["lmfcc"][:,channel] = lift
-
-
-    # read labels
-    res["labels"] = np.zeros([11])
-
-    with open(filename_cut + '.txt', 'r')   as fp:  
+    with open(filename_cut + '.txt', 'r') as fp:  
         lines = fp.readlines()        
         for l in lines:
             res["labels"][label_map[l[:3]]] = 1
-    print(res["labels"])            
 
     return res
 
-if __name__ == "__main__":
 
+def main():
+    for folder in Folders:
+        print("Entering folder ", folder)
+
+        features = []
+        for root, dirs, files in os.walk(folder):
+            total_files = len(files)/2
+
+            count = 0
+
+            for file in files[:100]:
+                if file.endswith('.wav'):
+                    count += 1
+                    progress_bar(count, total_files, suffix='')
+                    feat = extract_from_file(folder+file)
+                    features.append(feat)
+
+        np.save("%s_features" % folder[:23], features)
+
+
+if __name__ == "__main__":
     main()
+
